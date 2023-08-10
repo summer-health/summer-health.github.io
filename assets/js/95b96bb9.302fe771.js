91060eb84cd7c7805389d75f54a5ea78394860d8
github-pages <github-pages@summer-health.com>
2023-08-10 14:05:28 +0000

    Deploy website - based on c740c4c7142cb6370309b3e4688862f6164c6813

--- assets/js/95b96bb9.302fe771.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkexternal_blog=self.webpackChunkexternal_blog||[]).push([[561],{4577:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"A Better Provider Experience","permalink":"/blog/2023/08/10/care-os"},{"title":"Summer Health Engineering","permalink":"/blog/summer-health-engineering"}]}')}}]);
---
+++ assets/js/95b96bb9.e5cd5a67.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkexternal_blog=self.webpackChunkexternal_blog||[]).push([[561],{4577:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"A Better Healthcare Experience","permalink":"/blog/2023/08/10/care-os"},{"title":"Summer Health Engineering","permalink":"/blog/summer-health-engineering"}]}')}}]);
